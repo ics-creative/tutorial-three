771549be8cce1844d7cf14965e57246e44ed79dc
ics-ikeda <ikeda@ics-web.jp>
2019-02-11 22:06:59 +0900

    オフスクリーンキャンバスのサンプルを追加（リサイズ、ビットマップ）

---
+++ samples/osc_imagebitmaqp_worker.js
@@ -0,0 +1,53 @@
+importScripts(
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/101/three.min.js'
+);
+
+// メインスレッドから通達があったとき
+onmessage = async event => {
+  // メインスレッドからオフスクリーンキャンバスを受け取る
+  const canvas = event.data.canvas;
+  // Three.jsのライブラリの内部で style.width にアクセスされてしまう
+  // 対策しないと、エラーが発生するためダミーの値を指定
+  canvas.style = { width: 0, height: 0 };
+
+  // サイズを指定
+  const width = 960;
+  const height = 540;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(width, height);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(45, width / height);
+  camera.position.set(0, 0, +1000);
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(0, 1, 1);
+  scene.add(directionalLight);
+
+  // テクスチャーを読み込み
+  const texture = await new Promise(resolve => {
+    new THREE.ImageBitmapLoader().load('imgs/earthmap1k.jpg', imageBitmap => {
+      const texture = new THREE.CanvasTexture(imageBitmap);
+      resolve(texture);
+    });
+  });
+  // マテリアルを作成
+  const material = new THREE.MeshStandardMaterial({ map: texture });
+  const geometry = new THREE.SphereGeometry(300, 50, 50);
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  tick();
+
+  // 毎フレーム時に実行されるループイベントです
+  function tick() {
+    // カメラの自動移動
+    mesh.rotation.y += 0.01;
+
+    // レンダリング
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+  }
+};
---
+++ samples/osc_resize_worker.js
@@ -0,0 +1,68 @@
+importScripts(
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/101/three.min.js'
+);
+let renderer;
+let camera;
+
+// メインスレッドから通達があったとき
+onmessage = event => {
+  switch (event.data.type) {
+    case 'init':
+      init(event);
+      break;
+    case 'resize':
+      resize(event.data.width, event.data.height, event.data.devicePixelRatio);
+      break;
+  }
+};
+
+function init(event) {
+  // メインスレッドからオフスクリーンキャンバスを受け取る
+  const canvas = event.data.canvas;
+  // スクリーン情報を受け取る
+  const width = event.data.width;
+  const height = event.data.height;
+  const devicePixelRatio = event.data.devicePixelRatio;
+  // Three.jsのライブラリの内部で style.width にアクセスされてしまう
+  // 対策しないと、エラーが発生するためダミーの値を指定
+  canvas.style = { width: 0, height: 0 };
+
+  // レンダラーを作成
+  renderer = new THREE.WebGLRenderer({ canvas });
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(45, width / height);
+  camera.position.set(0, 0, 1000);
+  resize(width, height, devicePixelRatio);
+
+  // 球体を作成
+  const geometry = new THREE.SphereGeometry(300, 30, 30);
+  // マテリアルを作成
+  const material = new THREE.MeshBasicMaterial({ wireframe: true });
+  // メッシュを作成
+  const mesh = new THREE.Mesh(geometry, material);
+  // 3D空間にメッシュを追加
+  scene.add(mesh);
+
+  tick();
+
+  // 毎フレーム時に実行されるループイベントです
+  function tick() {
+    mesh.rotation.y += 0.01;
+
+    // レンダリング
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+  }
+}
+
+function resize(width, height, devicePixelRatio) {
+  // レンダラーのサイズを調整する
+  renderer.setPixelRatio(devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // カメラのアスペクト比を正す
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+}
